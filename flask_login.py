df2a56de0414b92b54c62a192260c35e0c84e55c
muthoni12 <muthoningarachu02@gmail.com>
2021-07-02 04:48:02 -0700

    initial commit

--- flask_login.py
+++ flask_login.py
@@ -1,18 +1,47 @@
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, redirect, url_for, render_template
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('login.html')
+
+
 @app.route('/login',methods=['GET', 'POST'])
 def login():
     username = ''
     if request.method == 'GET':
         username = request.args.get('username')
+    
     else:
         username = request.form['username']
 
-    return redirect(url_for('success', name = username))
+    return redirect(url_for('success', username = username))
 
-@app.route('/success/<name>')
-def success(name):
-    return 'welcome %s' % name
+# home page
+@app.route('/success/<username>')
+def success(username):
+    units = ['name', 'python', 'java', 'c#']
+    results = [
+        {
+        'name': 'muthoni',
+        'python': 98,
+        'java': 100,
+        'ç#': 110
+        },
+        {
+        'name': 'sam',
+        'python': 5,
+        'java': 10,
+        'ç#': 15
+        },
+        {
+        'name': 'maggy',
+        'python': 123,
+        'java': 789,
+        'ç#': 1890
+        }
+    ]
+    return render_template('success.html', username = username, subjects = units, results = results)
 
 if __name__=='__main__':
     app.run()
